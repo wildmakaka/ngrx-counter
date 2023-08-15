8e607bbf4259d0fd88d1709de6f683a68193ebdd
Marley <marley.nodejs@gmail.com>
2023-08-15 14:06:45 +0300

    11. Install Redux Chrome Extension and ngrx store dev tools for debugging Angular Ngrx Application

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 import { counterReducer } from 'src/app/counter/counter/state/counter.reducer';
 import { AppComponent } from './app.component';
 import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
@@ -10,9 +12,8 @@ import { CounterOutputComponent } from './counter/counter-output/counter-output.
 import { CounterComponent } from './counter/counter/counter.component';
 import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
 import { HomeComponent } from './home/home.component';
-import { AppRoutingModule } from 'src/app/app-routing.module';
-import { HeaderComponent } from './shared/component/header/header.component';
 import { PostsListComponent } from './posts/posts-list/posts-list.component';
+import { HeaderComponent } from './shared/component/header/header.component';
 
 @NgModule({
   imports: [
@@ -20,6 +21,13 @@ import { PostsListComponent } from './posts/posts-list/posts-list.component';
     AppRoutingModule,
     FormsModule,
     StoreModule.forRoot({ counter: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+    }),
   ],
   declarations: [
     AppComponent,
