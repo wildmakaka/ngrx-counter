0f1ba811d4a96dbcf1af4a176e4c1aca19e9b023
Marley <marley.nodejs@gmail.com>
2023-08-15 09:14:49 +0300

    06. Refactoring Store state. Using Async Pipe for selecting observable state data - Angular NGRX

--- app/src/app/counter/counter-output/counter-output.component.ts
+++ app/src/app/counter/counter-output/counter-output.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { CounterState } from 'src/app/counter/counter/state/counter.state';
 
 @Component({
   selector: 'app-counter-output',
@@ -7,13 +9,11 @@ import { Store } from '@ngrx/store';
   styleUrls: ['./counter-output.component.css'],
 })
 export class CounterOutputComponent implements OnInit {
-  counter: number;
+  counter$: Observable<{ counter: number }>;
 
-  constructor(private store: Store<{ counter: { counter: number } }>) {}
+  constructor(private store: Store<{ counter: CounterState }>) {}
 
   ngOnInit(): void {
-    this.store.select('counter').subscribe((data) => {
-      this.counter = data.counter;
-    });
+    this.counter$ = this.store.select('counter');
   }
 }
