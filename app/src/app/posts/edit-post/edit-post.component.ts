81c6dbe3363af2eef6cea5b7c4175acf36056daa
Marley <marley.nodejs@gmail.com>
2023-08-17 11:46:26 +0300

    17. Props in Ngrx Selectors. Sending arguments to the state selectors for updating Post

---
+++ app/src/app/posts/edit-post/edit-post.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { Post } from 'src/app/models/posts.model';
+import { getPostById } from 'src/app/posts/state/posts.selector';
+import { AppState } from 'src/app/store/app.state';
+
+@Component({
+  selector: 'app-edit-post',
+  templateUrl: './edit-post.component.html',
+  styleUrls: ['./edit-post.component.css'],
+})
+export class EditPostComponent implements OnInit, OnDestroy {
+  post: Post;
+  postForm: FormGroup;
+  postSubscription: Subscription;
+
+  constructor(private route: ActivatedRoute, private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      this.postSubscription = this.store
+        .select(getPostById, { id })
+        .subscribe((data) => {
+          this.post = data;
+          this.createForm();
+        });
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.postSubscription) {
+      this.postSubscription.unsubscribe();
+    }
+  }
+
+  createForm() {
+    this.postForm = new FormGroup({
+      title: new FormControl(this.post.title, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      description: new FormControl(this.post.description, [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+    });
+  }
+}
