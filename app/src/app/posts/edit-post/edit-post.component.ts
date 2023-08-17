b3ac53d7c30d242f55e0e89043b174cbca2c9735
Marley <marley.nodejs@gmail.com>
2023-08-17 12:46:34 +0300

    18. Update Post in Ngrx store immutable with actions in Angular Ngrx

--- app/src/app/posts/edit-post/edit-post.component.ts
+++ app/src/app/posts/edit-post/edit-post.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { Post } from 'src/app/models/posts.model';
+import { updatePost } from 'src/app/posts/state/posts.actions';
 import { getPostById } from 'src/app/posts/state/posts.selector';
 import { AppState } from 'src/app/store/app.state';
 
@@ -17,7 +18,11 @@ export class EditPostComponent implements OnInit, OnDestroy {
   postForm: FormGroup;
   postSubscription: Subscription;
 
-  constructor(private route: ActivatedRoute, private store: Store<AppState>) {}
+  constructor(
+    private route: ActivatedRoute,
+    private store: Store<AppState>,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
@@ -48,5 +53,23 @@ export class EditPostComponent implements OnInit, OnDestroy {
         Validators.minLength(10),
       ]),
     });
+  }
+
+  onSubmit() {
+    if (!this.postForm.valid) {
+      return;
+    }
+
+    const title = this.postForm.value.title;
+    const description = this.postForm.value.description;
+
+    const post: Post = {
+      id: this.post.id,
+      title,
+      description,
+    };
+
+    this.store.dispatch(updatePost({ post }));
+    this.router.navigate(['posts']);
   }
 }
