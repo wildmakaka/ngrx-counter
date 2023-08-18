ee6fa025d9c597ba5f903068058ea5b92a52ef76
Marley <marley.nodejs@gmail.com>
2023-08-18 11:58:50 +0300

    27. Save the User token data from Login in Ngrx State

--- app/src/app/services/auth.service.ts
+++ app/src/app/services/auth.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/app/environment';
+import { AuthResponseData } from 'src/app/models/AuthResponseData.module';
+import { User } from 'src/app/models/user.model';
 
 @Injectable({
   providedIn: 'root',
@@ -8,10 +11,23 @@ import { environment } from 'src/app/environment';
 export class AuthService {
   constructor(private http: HttpClient) {}
 
-  login(email: string, password: string) {
-    return this.http.post(
+  login(email: string, password: string): Observable<AuthResponseData> {
+    return this.http.post<AuthResponseData>(
       `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.FIREBASE_API_KEY}`,
-      { email, password, returnSecureTorken: true }
+      { email, password, returnSecureToken: true }
     );
+  }
+
+  formatUser(data: AuthResponseData) {
+    const expirationDate = new Date(
+      new Date().getTime() + +data.expiresIn * 1000
+    );
+    const user = new User(
+      data.email,
+      data.idToken,
+      data.localId,
+      expirationDate
+    );
+    return user;
   }
 }
