3859d78129d283c5330e3bfdf68034b6a2fc66d1
Marley <marley.nodejs@gmail.com>
2023-08-18 03:03:11 +0300

    25. Adding Auth Feature State for the Auth Module

--- app/src/app/auth/auth.module.ts
+++ app/src/app/auth/auth.module.ts
@@ -2,6 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { AuthReducer } from 'src/app/auth/state/auth.reducer';
+import { AUTH_STATE_NAME } from 'src/app/auth/state/auth.selector';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
@@ -15,7 +18,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    StoreModule.forFeature(AUTH_STATE_NAME, AuthReducer),
+    RouterModule.forChild(routes),
+  ],
   declarations: [LoginComponent],
 })
 export class AuthModule {}
